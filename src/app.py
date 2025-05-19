dc22982b90a7c70f427c46fb1f9b652d72f3c11f
saeed <saeedbabakan10@gmail.com>
2025-05-19 06:14:23 -0700

    add Pin Code Generator to the Dashboard

--- src/app.py
+++ src/app.py
@@ -6,3 +6,11 @@ from src.password_generators import RandomPasswordGenerator, MemorablePasswordGe
 
 st.image('https://cdn.dribbble.com/userupload/42306708/file/original-042d10b6579c7fb485a001087f065041.png?resize=768x576&vertical=center', width=300)
 st.title('Password Generator Dashboard')
+options = st.radio(
+        'Password type:',
+            options=['RandomPasswordGenerator', 'MemorablePasswordGenerator', 'PinCodeGenerator']
+)
+if options == 'PinCodeGenerator':
+    length = st.slider('length o the password', 0, 10, 4)
+    generator = PinCodeGenerator(length)
+    st.write(generator.generate())
