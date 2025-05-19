3138fb0f26dbfb9f79a0138159e4f5dadd5a08fd
saeed <saeedbabakan10@gmail.com>
2025-05-19 02:42:15 -0700

    initialize the dashboard and add image

---
+++ src/app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+from nltk.corpus import words
+
+from src.password_generators import RandomPasswordGenerator, MemorablePasswordGenerator, PinCodeGenerator
+
+
+st.image('https://cdn.dribbble.com/userupload/42306708/file/original-042d10b6579c7fb485a001087f065041.png?resize=768x576&vertical=center', width=300)
